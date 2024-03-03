8d68406da9e05d4936d37dd2afc665aa41d33d0e
dwirzkid <dwirizki164@gmail.com>
2024-03-03 23:39:31 +0700

    Commit pertama: Menambahkan Pertmuan1.php

---
+++ Pertmuan1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
+</head>
+
+<body>
+    <h2>Silahkan perkenalkan diri anda</h2>
+    <hr color="blue">
+    <?php
+    print "<b><font face='arial' size=5color='sienna'>Momentumtahunbaru memang palingenakuntukdijadikanturningperbaikan diri<br>";
+    print "But how? Begitu banyakkebiasaanburuk,begitubanyakmasalah,
+        begitubanyaktujuan,<br>";
+    echo "begitubanyakmimpi,bla...bla...bla...Stop! tentukan prioritas...firstthing
+        first!</font></b>";
+    ?>
+
+    <?php
+    print "<pre>";
+    print "<center><fontsize=14color='magenta'>BIODATA</font></center>";
+    echo "<hrsize=4width=60%color='blue'>";
+    print "<fontsize=12 color='red'>
+ </font><br>";
+    print "<fontsize=12 color='blue'>
+ </font><br>";
+    print "<fontsize=12 color='green'>
+ Informatika</font><br>";
+    print "<fontsize=12 color='purple'>
+ </font><br></pre>";
+    echo "<hrsize=4width=60%color='blue'>";
+    ?>
+</body>
+
+</html>
