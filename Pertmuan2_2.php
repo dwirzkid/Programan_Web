8ece2f5757f0203b6e50f7760ff4bae31543c91e
dwirzkid <dwirizki164@gmail.com>
2024-03-09 16:16:15 +0700

    Pertemuan 2 no 2 add

---
+++ Pertmuan2_2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 2 no 2</title>
+</head>
+<body>
+<center>
+  <br>
+  <br>
+<form action="" method="post">
+            Nilai 1 
+            <input type="text" name="n1" placeholder="">
+            <select name="choic" id="choic">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            Nilai 2
+            <input type="text" name="n2" placeholder="">
+            <input type="submit" value="Submit">
+        </form>
+        </br>
+</center>
+        <?php   
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nilai1 = $_POST['n1'];
+        $nilai2 = $_POST['n2'];
+        $pilih = $_POST['choic'];
+    
+        if($pilih == "+") {
+            $hasil = $nilai1 + $nilai2;
+        } else if($pilih == "-") {
+            $hasil = $nilai1 - $nilai2;
+        } else if($pilih == "*") {
+            $hasil = $nilai1 * $nilai2;
+        } else if($pilih == "/") {
+            $hasil = $nilai1 / $nilai2;
+        }
+        echo "<center><font size='3'>Hasil : $hasil </font></center>";
+    }
+?>
+    
+</body>
+</html>
